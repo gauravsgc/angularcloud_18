0dbabbcc4ebf655f992cb51d5d45215b6a0da78b
gauravsgc <pgauravpandeydit@gmail.com>
2024-11-05 08:05:13 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent,SidebarComponent],
+  imports: [HeaderComponent,FooterComponent,SidebarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,6 +18,18 @@ export class AppComponent {
   number_1:number=10;
   c:string="custom";
   address:any=signal('andhra');
+
+  number_2:number=100;
+
+  day:number=4;
+
+  names:string[]=['shazia','hemlatha','krishna'];
+
+employeedetails=[
+  {empId:101,name:'shazia',designation:'angular devloper'},
+  {empId:102,name:'hemlatha',designation:'node devloper'},
+  {empId:103,name:'krishna',designation:'full stack devloper'}
+]
 
   incrementData():any{
     // alert("goodmorning");
